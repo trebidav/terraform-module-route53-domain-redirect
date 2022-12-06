3368210d142a7d7cf76e6a29133f0c75bd1852c1
David Trebicky <david.trebicky@skippay.cz>
2022-12-06 11:50:25 +0100

    upgrade to new >0.13 tf format

--- acm.tf
+++ acm.tf
@@ -9,6 +9,6 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn         = "${aws_acm_certificate.cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation_1.fqdn}", "${aws_route53_record.cert_validation_2.fqdn}"]
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation_1.fqdn, aws_route53_record.cert_validation_2.fqdn]
 }
