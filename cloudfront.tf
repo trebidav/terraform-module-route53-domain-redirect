6cafdd340aa4dc95c5948569a5724b426ec40b60
John Casillas <jcasillas@gofigg.com>
2023-04-03 10:09:48 -0600

    Security Addition: Adding headers policy to pass mozilla test.

--- cloudfront.tf
+++ cloudfront.tf
@@ -52,4 +52,39 @@ resource "aws_cloudfront_distribution" "redirect" {
 
   wait_for_deployment = false
   depends_on          = [aws_acm_certificate_validation.validation]
+  response_headers_policy_id = aws_cloudfront_response_headers_policy.main.id
+}
+
+
+# https://dev.to/aws-builders/apply-cloudfront-security-headers-policy-with-terraform-fd3
+resource "aws_cloudfront_response_headers_policy" "main" {
+  name = "${terraform.workspace}-advertiser-security-header-policy"
+  security_headers_config {
+    content_type_options {
+      override = true
+    }
+    frame_options {
+      frame_option = "DENY"
+      override = true
+    }
+    referrer_policy {
+      referrer_policy = "same-origin"
+      override = true
+    }
+    xss_protection {
+      mode_block = true
+      protection = true
+      override = true
+    }
+    strict_transport_security {
+      access_control_max_age_sec = "63072000"
+      include_subdomains = true
+      preload = true
+      override = true
+    }
+    content_security_policy {
+      content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+      override = true
+    }
+  }
 }
