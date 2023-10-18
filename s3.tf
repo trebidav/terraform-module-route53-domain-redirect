0ac9e87dd4368c58f11ef1a4fddf894adb637b3f
David Trebicky <david.trebicky@skippay.cz>
2023-10-18 18:02:42 +0200

    todo

--- s3.tf
+++ s3.tf
@@ -5,9 +5,35 @@ resource "random_string" "hash" {
 
 resource "aws_s3_bucket" "redirect_bucket" {
   bucket = "redirect-${var.zone}-${lower(random_string.hash.result)}"
-  acl    = "public-read"
 
   website {
     redirect_all_requests_to = var.target_url
   }
+}
+
+data "aws_iam_policy_document" "public_read" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.build_bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.build_bucket.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.redirect_bucket.id
+  policy = data.aws_iam_policy_document.public_read.json
 }
