9db90ade74ab2ba2e7a3b9ddc8a394a3b9da3a1c
Andrew Slack <me@andyslack.com>
2024-02-26 16:11:13 +0100

    fix issue with deprecated Argument acl    = "public-read"

--- s3.tf
+++ s3.tf
@@ -5,9 +5,34 @@ resource "random_string" "hash" {
 
 resource "aws_s3_bucket" "redirect_bucket" {
   bucket = "redirect-${var.zone}-${lower(random_string.hash.result)}"
-  acl    = "public-read"
 
   website {
     redirect_all_requests_to = var.target_url
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "redirect_bucket" {
+  bucket = aws_s3_bucket.redirect_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "redirect_bucket" {
+  bucket = aws_s3_bucket.redirect_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "redirect_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.redirect_bucket,
+    aws_s3_bucket_public_access_block.redirect_bucket,
+  ]
+
+  bucket = aws_s3_bucket.redirect_bucket.id
+  acl    = "public-read"
 }
