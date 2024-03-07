56cf1df934fa4290fc63a8a17d6e8d7bd48c2f77
Radim Sückr <radim.suckr@skippay.cz>
2024-03-07 10:42:34 +0100

    refactor(route53)!: use for_each for cert validations

--- acm.tf
+++ acm.tf
@@ -10,5 +10,5 @@ resource "aws_acm_certificate" "cert" {
 
 resource "aws_acm_certificate_validation" "validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [ for record in aws_route53_record.cert_validation: record.fqdn ]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
